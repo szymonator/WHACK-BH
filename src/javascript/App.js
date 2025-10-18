a1c809c5e06a526dffeebb3ccc92100230cfca5d
Dahmed234 <115463196+Dahmed234@users.noreply.github.com>
2025-10-18 15:46:33 +0100

    Textbox client side checking + request checking

--- src/javascript/App.js
+++ src/javascript/App.js
@@ -1,22 +1,21 @@
 import logo from '../assets/logo.svg';
 import '../css/App.css';
+import { MyTextbox } from './MyTextbox';
 
 function App() {
   return (
     <div className="App">
+      <h1>
+        Amazon Review Checker
+      </h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          insert an amazon link
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <MyTextbox/>
+
       </header>
     </div>
   );
---
+++ src/javascript/MyTextbox.js
@@ -0,0 +1,84 @@
+
+import React, { useState } from "react";
+export function MyTextbox() {
+
+  // state for textbox
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value)
+  }
+
+
+
+
+  function handleClick() {
+    if (validURL(inputValue)) {
+      console.log("hell yea");
+    }
+
+    else {
+      console.log(":(")
+    }
+
+
+
+
+    console.log("presto vro");
+  }
+
+  const VALIDHOSTS = ["amazon.co.uk", "amazon.com"];
+  // returns whether the given input is a valid url
+  function validURL(userURL) {
+    // if url malformed, URL constructor throws an error
+    try {
+      const url = new URL(userURL);
+
+      console.log(url.host)
+      //return whether host is within allowed list
+      return VALIDHOSTS.includes(url.host);
+
+    }
+    // return false if url was malformed
+    catch {
+      return false
+    }
+
+
+
+
+  }
+
+
+  async function sendRequest(link) {
+    const url = ""
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: link
+      })
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`)
+      }
+
+      return await response.json()
+
+    }
+    
+    catch (error) {
+      console.error(error.message)
+    }
+
+  }
+
+
+  return (
+
+    <div className="textbox">
+      <input type="text" value={inputValue} onChange={handleInputChange}>
+      </input>
+      <input type='button' onClick={handleClick}></input>
+    </div>
+  );
+}
