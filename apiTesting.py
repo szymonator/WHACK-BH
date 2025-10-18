c9767c4b312bfab5517ceb64a6ff2803c3fe4bdd
szymonator <23020097@woodhouse.ac.uk>
2025-10-19 00:53:27 +0100

    Added image address retrieval

--- apiTesting.py
+++ apiTesting.py
@@ -1,4 +1,3 @@
-# imports
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,8 +10,9 @@ analyzer = SentimentIntensityAnalyzer()
 def getInfo():
     driver = webdriver.Chrome()
     driver.get("https://www.ebay.co.uk/itm/146870704806?_skw=cups+and+mugs&itmmeta=01K7VV4KM2C7H3QE649MWA28J9&hash=item22322d1aa6:g:Y0wAAeSwwfdo361J&itmprp=enc%3AAQAKAAAA0NHOg0D50eDiCdi%2FfP0r02ttbuuZoIVydl20m9w6AMUa5dQzJZZxkcjxVDalk7p%2FXl0NceH7VEocqOagEYJBMCqRniv%2Bbs%2BiN5vXy1R8hKPfDD8t9qlJn2KFT%2FzloHyDOTha%2B0vcxAdtv%2B9iB8sKfYi5dzh5efuJuXUMKgcAvqW5EA1KvYinGI%2FPguu4suWcEnOmVYy8ltRbFFSgfdydXoLu9V2HbbhSnW1oSTERCG1ASWw7QvfcHCnFnz1W%2FQ1UjAm%2B5Svcsxa8x2GhhjotNEI%3D%7Ctkp%3ABk9SR5a6kvu-Zg")
+    wait = WebDriverWait(driver, 10)
     try:
-        WebDriverWait(driver, 10).until(
+        wait.until(
             EC.presence_of_element_located((By.CLASS_NAME, "fdbk-container__details__comment"))
         )
     except:
@@ -20,6 +20,8 @@ def getInfo():
         driver.quit()
         return
     
+    image = driver.find_element(By.XPATH, "/html/body/div[2]/main/div[1]/div[1]/div[4]/div/div/div[1]/div[1]/div/div[1]/div[1]/div[2]/div[4]/div[1]/img")
+    imageAdress = image.get_attribute('src')
     html = driver.page_source
 
 
@@ -29,6 +31,7 @@ def getInfo():
     product_info["Name"] = driver.title
     soup = BeautifulSoup(html, 'html.parser')
     product_info["Comment"] = soup.find_all(class_="fdbk-container__details__comment") # problem here: it pulls comments for the seller too
+    product_info["Image"] = soup.find()
     # product_info[""]
 
     print(product_info)
